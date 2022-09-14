8327a828c5d8f366034298e7b669a7ccae9b18b0
HenriquePRA <henriquep.r@hotmail.com>
2022-09-13 21:29:16 -0300

    project init

---
+++ src/main/java/tech/hpoggi/kilobytebank/KilobytebankApplication.java
@@ -0,0 +1,13 @@
+package tech.hpoggi.kilobytebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KilobytebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KilobytebankApplication.class, args);
+	}
+
+}
