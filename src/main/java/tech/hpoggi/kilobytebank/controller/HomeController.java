c0557d1ae9fa88f632c716582b8c9f2580c7678f
HenriquePRA <henriquep.r@hotmail.com>
2022-09-13 23:05:27 -0300

    hello kilobytebank

---
+++ src/main/java/tech/hpoggi/kilobytebank/controller/HomeController.java
@@ -0,0 +1,12 @@
+package tech.hpoggi.kilobytebank.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/home")
+    public String showHomePage() {
+        return "index";
+    }
+}
